b3f3f7a7da05b61f94ea3c3b6ce3993a0bb826d2
Xxwwzz12 <konezvsem@gmail.com>
2025-09-30 21:12:21 +0500

    вернулись к старым ошибкам

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { htmlInjectionPlugin } from 'vite-plugin-html-injection';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    htmlInjectionPlugin({
-      injections: [
-        {
-          name: 'Telegram WebApp SDK',
-          type: 'raw',
-          injectTo: 'head',
-          path: './src/injections/telegram-sdk.html', // Создайте этот файл
-          buildModes: 'both',
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
