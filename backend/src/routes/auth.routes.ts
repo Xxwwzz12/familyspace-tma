96e2b48b191bd659535b09bd36eabde0eb84d1da
Xxwwzz12 <konezvsem@gmail.com>
2025-09-30 09:21:49 +0500

    добавил рут

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authInit, testAuth } from '../controllers/auth.controller';
+import { testHashValidation } from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/init', authInit);
 router.post('/test', testAuth);
+router.post('/auth/test-hash', testHashValidation);
 
 export default router;
