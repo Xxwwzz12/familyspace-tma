c71f925f1ebbd90224ad7a32292d88e5fb801efb
Xxwwzz12 <konezvsem@gmail.com>
2025-10-02 14:02:59 +0500

    все еще бъемся

--- backend/api/ping.ts
+++ backend/api/ping.ts
@@ -2,5 +2,6 @@
 export default function handler(req: any, res: any) {
   res.setHeader('x-test-ping', '1');
   res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
   res.end(JSON.stringify({ ok: true, time: new Date().toISOString() }));
 }
