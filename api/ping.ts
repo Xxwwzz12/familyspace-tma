248362353a22839cf1c1720f16bb6c520ced2ed3
Xxwwzz12 <konezvsem@gmail.com>
2025-10-02 10:21:21 +0500

    удалили все temp.txt и добавили ping.ts для теста

---
+++ api/ping.ts
@@ -0,0 +1,7 @@
+// api/ping.ts
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('x-test-ping', '1');
+  res.json({ ok: true, time: new Date().toISOString() });
+}
